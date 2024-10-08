520b3151df69dee868a3d9576abbabc72439bdd6
Jarret Diep <jarrettcdi@gmail.com>
2024-10-08 13:34:55 -0700

    Added specifications and tests for 'sort' method

--- src/main/model/Inventory.java
+++ src/main/model/Inventory.java
@@ -7,28 +7,26 @@ import java.util.ArrayList;
 public class Inventory {
     private ArrayList<Item> inventory;
 
-    // EFFECT: creates an Inventory with no items inside of it
+    // EFFECT: creates an unsorted Inventory with no items inside of it
     public Inventory() {
         this.inventory = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECT: adds given item to this inventory, if already inside increases item quantity
-    public void addItem(Item item){
+    public void addItem(Item item) {
         this.inventory.add(item);
     }
 
-    // REQUIRES: given item is in inventory
     // MODIFIES: this
     // EFFECT: removes first instance of given item from the inventory
-    public void removeItem(Item item){
+    public void removeItem(Item item) {
         this.inventory.remove(item);
     }
 
-    // REQUIRES: given item is in inventory
     // MODIFIES: this
     // EFFECT: removes ALL instances of given item from the inventory
-    public void removeAllItem(Item item){
+    public void removeAllItem(Item item) {
         while (inventory.contains(item)){
             inventory.remove(item);
         }
@@ -36,13 +34,24 @@ public class Inventory {
 
     // MODIFIES: this
     // EFFECT: removes all items from inventory
-    public void clearInventory(){
+    public void clearInventory() {
         inventory.clear();
     }
 
-    // REQUIRES: given item is in inventory
+    // REQUIRES: sort is one of "Name", "Type", "Value", "Weight"
+    // MODIFIES: this
+    // EFFECT: sorts an inventory according to sort, and order is whether
+    //         ascending(true) or descending(false)
+    public void sort(String sort, Boolean order) {
+        // stub
+    }
+
+    public Item getItem(int index) {
+        return inventory.get(index);
+    }
+    
     // EFFECT: returns number of times given item appears in Inventory
-    public int getItemCount(Item item){
+    public int getItemCount(Item item) {
         int count = 0;
         for (Item i:inventory){
             if (i.equals(item)){
@@ -52,7 +61,7 @@ public class Inventory {
         return count;
     }
 
-    public int getNumItems(){
+    public int getNumItems() {
         int count = 0;
         for (Item i:inventory){
             count += 1;
--- src/test/model/InventoryTest.java
+++ src/test/model/InventoryTest.java
@@ -10,12 +10,28 @@ public class InventoryTest {
     Inventory testInventory;
     Item testItem;
     Item testWeapon;
+
+    Inventory testSortInventory;
+    Item testSort1;
+    Item testSort2;
+    Item testSort3;
+    Item testSort4;
     
     @BeforeEach
     void runBefore() {
         testInventory = new Inventory();
         testItem = new Item("Test Item", "Misc", 1, 5, "A test item.");
         testWeapon = new Item("Test Weapon", "Weapon", 10, 15, "A test weapon.");
+
+        testSortInventory = new Inventory();
+        testSort1 = new Item("A", "Weapon", 40, 2, "A test item.");
+        testSort2 = new Item("B", "Consumable", 30, 2, "A test item.");
+        testSort3 = new Item("C", "Armour", 20, 3, "A test item.");
+        testSort4 = new Item("D", "Misc", 10, 4, "A test item.");
+        testSortInventory.addItem(testSort2);
+        testSortInventory.addItem(testSort4);
+        testSortInventory.addItem(testSort1);
+        testSortInventory.addItem(testSort3);
     }
 
     @Test
@@ -38,14 +54,14 @@ public class InventoryTest {
     }
 
     @Test
-    void testRemoveItem(){
+    void testRemoveItem() {
         testInventory.addItem(testItem);
         testInventory.removeItem(testItem);
         assertEquals(0, testInventory.getNumItems());
     }
 
     @Test
-    void testRemoveMultItems(){
+    void testRemoveMultItems() {
         testInventory.addItem(testItem);
         testInventory.addItem(testWeapon);
 
@@ -58,7 +74,7 @@ public class InventoryTest {
     }
 
     @Test
-    void testRemoveAllItems(){
+    void testRemoveAllItems() {
         testInventory.addItem(testItem);
         testInventory.addItem(testItem);
         testInventory.addItem(testWeapon);
@@ -67,11 +83,91 @@ public class InventoryTest {
     }
 
     @Test
-    void testClearInventory(){
+    void testClearInventory() {
         testInventory.addItem(testItem);
         testInventory.addItem(testItem);
         testInventory.addItem(testWeapon);
         testInventory.clearInventory();
         assertEquals(0, testInventory.getNumItems());
+    }
+
+    @Test
+    void testSortAlphabetical() {
+        testSortInventory.sort("Name", true);
+
+        assertEquals(testSortInventory.getItem(0), testSort1);
+        assertEquals(testSortInventory.getItem(1), testSort2);
+        assertEquals(testSortInventory.getItem(2), testSort3);
+        assertEquals(testSortInventory.getItem(3), testSort4);
+    }
+
+    @Test
+    void testSortReverseAlphabetical() {
+        testSortInventory.sort("Name", false);
+
+        assertEquals(testSortInventory.getItem(0), testSort4);
+        assertEquals(testSortInventory.getItem(1), testSort3);
+        assertEquals(testSortInventory.getItem(2), testSort2);
+        assertEquals(testSortInventory.getItem(3), testSort1);
+    }
+
+    @Test
+    void testSortType() {
+        testSortInventory.sort("Type", true);
+
+        assertEquals(testSortInventory.getItem(0), testSort1);
+        assertEquals(testSortInventory.getItem(1), testSort3);
+        assertEquals(testSortInventory.getItem(2), testSort2);
+        assertEquals(testSortInventory.getItem(3), testSort4);
+    }
+
+    @Test
+    void testSortReverseType() {
+        testSortInventory.sort("Type", false);
+
+        assertEquals(testSortInventory.getItem(0), testSort4);
+        assertEquals(testSortInventory.getItem(1), testSort2);
+        assertEquals(testSortInventory.getItem(2), testSort3);
+        assertEquals(testSortInventory.getItem(3), testSort1);
+    }
+
+    @Test
+    void testSortValue() {
+        testSortInventory.sort("Type", true);
+
+        assertEquals(testSortInventory.getItem(0), testSort4);
+        assertEquals(testSortInventory.getItem(1), testSort3);
+        assertEquals(testSortInventory.getItem(2), testSort2);
+        assertEquals(testSortInventory.getItem(3), testSort1);
+    }
+
+    @Test
+    void testSortReverseValue() {
+        testSortInventory.sort("Type", false);
+
+        assertEquals(testSortInventory.getItem(0), testSort1);
+        assertEquals(testSortInventory.getItem(1), testSort2);
+        assertEquals(testSortInventory.getItem(2), testSort3);
+        assertEquals(testSortInventory.getItem(3), testSort4);
+    }
+
+    @Test
+    void testSortWeight() {
+        testSortInventory.sort("Type", true);
+
+        assertEquals(testSortInventory.getItem(0), testSort2);
+        assertEquals(testSortInventory.getItem(1), testSort1);
+        assertEquals(testSortInventory.getItem(2), testSort3);
+        assertEquals(testSortInventory.getItem(3), testSort4);
+    }
+
+    @Test
+    void testSortReverseWeight() {
+        testSortInventory.sort("Type", false);
+
+        assertEquals(testSortInventory.getItem(0), testSort4);
+        assertEquals(testSortInventory.getItem(1), testSort3);
+        assertEquals(testSortInventory.getItem(2), testSort1);
+        assertEquals(testSortInventory.getItem(3), testSort2);
     }
 }
